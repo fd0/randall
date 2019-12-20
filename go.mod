fb31b4e3e52c883259a4210220f7f67a67b99a2c
Alexander Neumann <alexander@bumpern.de>
2019-12-20 13:20:52 +0100

    Add mode to reconstruct password

--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/fd0/randall
 
 go 1.12
 
-require github.com/spf13/pflag v1.0.3
+require (
+	github.com/c-bata/go-prompt v0.2.3
+	github.com/lithammer/fuzzysearch v1.1.0
+	github.com/mattn/go-runewidth v0.0.4 // indirect
+	github.com/pkg/term v0.0.0-20190109203006-aa71e9d9e942 // indirect
+	github.com/spf13/pflag v1.0.3
+)
--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"math/big"
 	"os"
+	"sort"
 	"strings"
 	"unicode/utf8"
 
+	"github.com/c-bata/go-prompt"
 	"github.com/fd0/randall/wordlists"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 	"github.com/spf13/pflag"
 )
 
@@ -17,6 +20,7 @@ type Options struct {
 	Words       uint
 	Wordlist    string
 	Passphrases uint
+	Reconstruct bool
 }
 
 func die(msg string, args ...interface{}) {
@@ -62,6 +66,24 @@ func generate(n uint, list []string) string {
 	return strings.Join(words, "")
 }
 
+func completer(wordlist []string) prompt.Completer {
+	capitalize(wordlist)
+	return func(d prompt.Document) []prompt.Suggest {
+
+		matches := fuzzy.RankFindFold(d.GetWordBeforeCursor(), wordlist)
+		sort.Sort(matches)
+
+		var suggestions []prompt.Suggest
+		for _, match := range matches {
+			suggestions = append(suggestions, prompt.Suggest{
+				Text: match.Target,
+			})
+		}
+
+		return suggestions
+	}
+}
+
 func main() {
 	var opts Options
 
@@ -69,6 +91,7 @@ func main() {
 	flags.UintVarP(&opts.Words, "words", "w", 5, "generate passphrase with `n` words")
 	flags.UintVarP(&opts.Passphrases, "passphrases", "n", 1, "generate `n` passphrases")
 	flags.StringVarP(&opts.Wordlist, "wordlist", "l", "en", fmt.Sprintf("use `wordlist` as the source for words (valid: %v)", strings.Join(wordlists.Names(), ", ")))
+	flags.BoolVarP(&opts.Reconstruct, "reconstruct", "r", false, "interactively reconstruct a password based on a wordlist")
 
 	err := flags.Parse(os.Args)
 	if err == pflag.ErrHelp {
@@ -81,6 +104,54 @@ func main() {
 	list, err := wordlists.Get(opts.Wordlist)
 	if err != nil {
 		die("error: %v", err)
+	}
+
+	if opts.Reconstruct {
+		fmt.Printf("reconstruct password consisting of %d words using wordlist %s\n", opts.Words, opts.Wordlist)
+		fmt.Printf("\ntype first word, complete with <tab>, press <enter> to add word\n")
+
+		validWords := make(map[string]struct{})
+		for _, word := range list {
+			validWords[capitalizeWord(word)] = struct{}{}
+		}
+
+		prefix := "> "
+		var words []string
+
+		p := prompt.New(
+			func(word string) {
+				if word == "" {
+					fmt.Printf("aborting\n")
+					os.Exit(0)
+				}
+
+				if _, ok := validWords[word]; !ok {
+					fmt.Printf("word %q is not in wordlist, try again\n", word)
+					return
+				}
+
+				words = append(words, word)
+				fmt.Printf("add word %q to password\n", word)
+
+				if uint(len(words)) == opts.Words {
+					fmt.Printf("password is: %s\n", strings.Join(words, ""))
+					os.Exit(0)
+				}
+			},
+			completer(list),
+			prompt.OptionPrefix(prefix),
+			prompt.OptionLivePrefix(func() (string, bool) {
+				if len(words) == 0 {
+					return "", false
+				}
+
+				return strings.Join(words, "") + " > ", true
+			}),
+			prompt.OptionTitle("foo"),
+		)
+
+		p.Run()
+		return
 	}
 
 	for i := uint(0); i < opts.Passphrases; i++ {
